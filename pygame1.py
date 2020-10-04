5ea4b389797eae97563d193fd617b0ebaa900e08
Kaylor <kaylor@kali.Kali>
2020-10-04 20:29:12 +0330

    add left and right charcter car

--- pygame1.py
+++ pygame1.py
@@ -7,7 +7,7 @@ pygame.init()
 blue = (127, 255, 212)
 purple = (138, 43, 226)
 orange_red = (255, 69, 0)
-
+white = (255,255,255)
 #display resoulion   ====  width and height for game window
 display_width = 800
 display_height = 600
@@ -18,13 +18,33 @@ pygame.display.set_caption("Python Game Race")
 
 clock = pygame.time.Clock()
 
+carimg = pygame.image.load('gg.png')
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change = 0
+
+def car(x,y):
+    gameDisplay.blit(carimg,(x,y))
+
 crashed = False
 
 while not crashed:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5  
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0 
+            
+            
+    x += x_change          
+    gameDisplay.fill(white) 
+    car(x,y)   
     pygame.display.update()
     clock.tick(60)
     
